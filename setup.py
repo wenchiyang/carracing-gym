7629a3acf711419edc7e67ba941e59bfc4c2a8f4
wenchiyang <Wen-Chi.Yang@cs.kuleuven.be>
2024-01-17 17:39:04 +0100

    Add dependency versions

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='carracing_gym',
       version='0.0.1',
       install_requires=[
-            'gym',
+            'gym==0.21.0',
+            'pyglet==1.5.11',
             'gym[box2d]',
-            'pyglet',
             'pyvirtualdisplay'
       ] # And any other dependencies required
 )
+
